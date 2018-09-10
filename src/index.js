7f52d1d38a5d7c16977f4b8c107ab974c08fd05e
shivani Pitta <shivanipitta@shivanis-MBP.router>
2018-09-10 14:05:34 -0400

    added pxblue themes

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import './style.css';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import {EatonThemes} from '@pxblue/themes/react/src';
 
-render(<App />, document.getElementById('root'));
+render(
+  <MuiThemeProvider theme={createMuiTheme(EatonThemes.blue)}>
+    <App />
+  </MuiThemeProvider>
+  , document.getElementById('root'));
